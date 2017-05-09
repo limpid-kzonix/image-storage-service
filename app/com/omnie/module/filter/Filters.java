c147c9c6fd675df30b9d2d94addddd0702d923c6
Hezzer <balyszyn@gmail.com>
2017-05-09 12:28:17 +0300

    IMAGE-SERVICE: add cache api (caching images with different size during 15 minutes)

--- app/com/omnie/module/filter/Filters.java
+++ app/com/omnie/module/filter/Filters.java
@@ -1,22 +1,29 @@
 package com.omnie.module.filter;
 
+
 import com.google.inject.Inject;
+import play.filters.cors.CORSFilter;
 import play.http.HttpFilters;
 import play.mvc.EssentialFilter;
+
 
 /**
  * Created by limpid on 5/4/17.
  */
 public class Filters implements HttpFilters {
 
+
 	private EssentialFilter[] filters;
 
 	@Inject
-	public Filters(LoggingFilter loggingFilter) {
-		filters = new EssentialFilter[] { loggingFilter.asJava() };
+	public Filters( LoggingFilter loggingFilter, CORSFilter corsFilter ) {
+		filters = new EssentialFilter[]{
+				loggingFilter.asJava( ),
+				corsFilter.asJava( )
+		};
 	}
 
-	public EssentialFilter[] filters() {
+	public EssentialFilter[] filters( ) {
 		return filters;
 	}
 }
